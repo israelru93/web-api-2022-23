6025d875e5140f6d42aa3a05c12d70991087eb66
unknown <israelru93@gmail.com>
2022-12-20 19:35:57 +0200

    corcting bugs of server

--- api/v1/reuots/prodact.js
+++ api/v1/reuots/prodact.js
@@ -3,17 +3,27 @@
 const router=require("exspress").Router();
 
 router.get("/prodact",(req,res)=>{
-    return res.steatus(200).json({msg:"all prodact"});
+    return res.steatus(200).json({msg:" get all prodact"});
 });
 
 
 
-router.get("/prodact/:id",(req,res)=>{});
+router.get("/prodact/:id",(req,res)=>{
+    return res.steatus(200).json({msg:"prodact by id "});
+});
 
-router.put("/prodact/:id",(req,res)=>{});
+router.put("/prodact/:id",(req,res)=>{
+    return res.steatus(200).json({msg:"updeta prodact by id "});
 
-router.post("/prodact",(req,res)=>{});
+});
 
-router.Delete("/prodact/:id",(req,res)=>{});
+router.post("/prodact",(req,res)=>{
+
+    return res.steatus(200).json({msg:"add prodact"});
+});
+
+router.Delete("/prodact/:id",(req,res)=>{
+    return res.steatus(200).json({msg:"delete prodact by id "});
+});
 
 module.exports=reouter();
